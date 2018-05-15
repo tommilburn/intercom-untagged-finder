3220ce69fa98b0de7ca07f44d075ecadc884916e
Tom Milburn <thomasjosephmilburn@gmail.com>
2018-05-14 20:54:34 -0400

    basic intercom api set up

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("hello world");
+require('dotenv').config() //INTERCOM_TOKEN
 
+const Intercom = require("intercom-client");
+var client = new Intercom.Client({token: process.env.INTERCOM_TOKEN});
+
+var admins = []
+client.admins.list(parseAdmins);
+
+function parseAdmins(err, data){
+  if(err){
+    console.log(err);
+  } else {
+    admins = data.body.admins;
+    console.log(admins);
+  }
+}
